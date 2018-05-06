f894914e9aad96c8c884e106ecd0d2e08481fec2
Jacob Young <jwyoung07@gmail.com>
2018-05-05 21:26:19 -0700

    added all code instructed from powerpoints, still uses old render method

---
+++ src/Block.java
@@ -0,0 +1,48 @@
+public class Block {
+    private boolean IsActive
+            ;
+    private BlockType Type;
+    private float x,y,z
+            ;
+    public enum BlockType
+    {
+        BlockType_Grass(0),
+        BlockType_Sand(1),
+        BlockType_Water(2),
+        BlockType_Dirt(3),
+        BlockType_Stone(4),
+        BlockType_Bedrock(5),
+        BlockType_Default(6);
+        private int BlockID;
+        BlockType
+                (int i) {
+            BlockID
+                    =i;
+        }
+        public int GetID(){
+            return BlockID
+                    ;
+        }
+        public void SetID
+                (int i){
+            BlockID = i;
+        }
+    }
+    public Block(BlockType type){
+        Type= type;
+    }
+    public void setCoords(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public boolean IsActive() {
+        return IsActive;
+    }
+    public void SetActive(boolean active){
+        IsActive=active;
+    }
+    public int GetID(){
+        return Type.GetID();
+    }
+}
